2d451928291583914a044390bdb90c001f68d672
Domovski1 <624273242@mail.ru>
2021-04-04 20:47:01 +0300

    Добавлена логика к странице авторизации

--- WS2021/MedicineLab/Pages/SignIn.xaml.cs
+++ WS2021/MedicineLab/Pages/SignIn.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MedicineLab.Model;
+using System;
 using System.Windows;
+using System.Linq;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MedicineLab.Pages
 {
@@ -23,6 +15,92 @@ namespace MedicineLab.Pages
         public SignIn()
         {
             InitializeComponent();
+        }
+
+        #region Methods 
+
+        /// <summary>
+        /// Метод, позволяющий создавать новую капчу
+        /// </summary>
+        /// <returns></returns>
+        string CreateCapthca()
+        {
+            string password = "",
+                   Alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            Random rand = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                password += Alphabet[rand.Next(Alphabet.Length)];
+            }
+
+            return password;  
+        }
+
+        #endregion
+
+        // Создание новой капчи
+        private void BtnRecaptcha_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            TblockCaptcha.Text = CreateCapthca();
+        }
+
+        // Смена масок пароля
+        private void BtnShowPassword_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (Pbox.Visibility == Visibility.Visible)
+            {
+                TxbPassword.Text = Pbox.Password;
+                Pbox.Visibility = Visibility.Collapsed;
+                TxbPassword.Visibility = Visibility.Visible;
+            } else
+            {
+                Pbox.Visibility = Visibility.Visible;
+                TxbPassword.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            Visit Guest;
+            if (TxbCaptcha.Text == TblockCaptcha.Text)
+            {
+                var CurrentUser = AppData.db.User.FirstOrDefault(x => x.Login == TxbLogin.Text);
+                if (CurrentUser != null)
+                {
+                    if (TxbLogin.Text == CurrentUser.Login && Pbox.Password == CurrentUser.Password)
+                    {
+                        Guest = new Visit
+                        {
+                            Success = true,
+                            TimeVisited = DateTime.Now,
+                            UserID = CurrentUser.ID,
+                        };
+                        AppData.db.Visit.Add(Guest);
+                        AppData.db.SaveChanges();
+                        //NavigationService.Navigate(new );
+                    } else
+                    {
+                        Guest = new Visit
+                        {
+                            Success = false,
+                            TimeVisited = DateTime.Now,
+                            UserID = CurrentUser.ID,
+                        };
+                        AppData.db.Visit.Add(Guest);
+                        AppData.db.SaveChanges();
+                        MessageBox.Show("Неверный логин или пароль!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                } else
+                {
+                    MessageBox.Show("Пользователя с такими данными нет в базе!", "404", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+
+            } else
+            {
+                MessageBox.Show("Неверно введена капча!", "Ошибка ввода!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
