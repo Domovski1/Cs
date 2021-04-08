1cf185af2216a4d39cfe821263bee1cf91a2a2d9
Domovski1 <624273242@mail.ru>
2021-04-08 22:59:44 +0300

    SomeChanges

---
+++ WS2021/MedicineLab/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MedicineLab.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedicineLab.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public User user { get; set; }
+        public WelcomePage(User GetUser)
+        {
+            InitializeComponent();
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            TxbHello.Text = $"Добро пожаловать {user.FirstName} {user.LastEnter}! Ваша роль в системе: {user.Role.Title}";
+            if (user.RoleID == 1)
+            {
+                ImgBox.Source = new BitmapImage(new Uri("pack:"));
+            }
+        }
+
+        private void BtnToNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
