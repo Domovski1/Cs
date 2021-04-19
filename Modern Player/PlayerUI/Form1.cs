312e7eea2381fa81f4084ffaa3eff07b4f92c7b6
Domovski1 <624273242@mail.ru>
2021-04-19 12:37:01 +0300

    Новый плеер с доп меню

---
+++ Modern Player/PlayerUI/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayerUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            hideSubMenu();
+        }
+
+        private void hideSubMenu()
+        {
+            panelMediaSubMenu.Visible = false;
+            panelPlaylistSubMenu.Visible = false;
+            panelToolsSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelMediaSubMenu);
+        }
+
+        #region MediaSubMenu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //openChildForm(new Form2());
+            //..
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    mp3Player.open(file.FileName);
+                }
+            }
+            //..
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+        #endregion
+
+        private void btnPlaylist_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelPlaylistSubMenu);
+        }
+
+        #region PlayListManagemetSubMenu
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+        #endregion
+
+        private void btnTools_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelToolsSubMenu);
+        }
+        #region ToolsSubMenu
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+        #endregion
+
+        private void btnEqualizer_Click(object sender, EventArgs e)
+        {
+            //openChildForm(new Form3());
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            Process.Start("https://vk.com/gabiyn");
+            hideSubMenu();
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null) activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+
+        Mp3Player mp3Player = new Mp3Player();
+        bool IsPlay = false;
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            IsPlay = !IsPlay;
+            if (IsPlay)
+            {
+                mp3Player.play();
+            } else
+            {
+                mp3Player.stop();
+            }
+        }
+
+        public static class NativeMethods
+        {
+
+            [DllImport("winmm.dll")]
+            internal static extern int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
+            [DllImport("winmm.dll")]
+            internal static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+        } // Специальный класс для работы прогарммы с аудио
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            uint CurrVol = ushort.MaxValue / 2;
+            NativeMethods.waveOutGetVolume(IntPtr.Zero, out CurrVol);
+            ushort CalcVol = (ushort)(CurrVol & 0x0000ffff);
+            int NewVolume = ((ushort.MaxValue / 100) * trackBar1.Value);
+            uint NewVolumeAllChannels = (((uint)NewVolume & 0x0000ffff) | ((uint)NewVolume << 16));
+            NativeMethods.waveOutSetVolume(IntPtr.Zero, NewVolumeAllChannels);
+            label3.Text = Convert.ToString("Volume: " + trackBar1.Value + "%");
+        } // Трекбар регулирующий уровень громкости аудио
+    }
+    
+}
